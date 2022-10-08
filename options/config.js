7fdba980665e4479c2acf0f1e4801ca324189f31
Hexxin33 <gamingacc140@gmail.com>
2022-10-08 20:43:10 +0700

    Bot4

---
+++ options/config.js
@@ -0,0 +1,60 @@
+/* 
+NOTE : JGN HPS DIBAWAH INI!!
+RECODE : RAHMXBOT X RAHMAN GNTENG:V
+WHATSAPP : 085821676621
+NOMOR BOT : 085821369324
+NOTE : JGN HPS AUTHORNYA!!!
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//━━━━━━━━━━━━━━━[ WEBSITE APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIs = {
+liaacans: 'https://zenzapis.xyz',
+}
+
+//━━━━━━━━━━━━━━━[ APIKEY ]━━━━━━━━━━━━━━━━━//
+
+global.APIKeys = {
+'https://zenzapis.xyz': '0e92565522',
+}
+
+//━━━━━━━━━━━━━━━[ SETTING ]━━━━━━━━━━━━━━━━━//
+global.thumb = fs.readFileSync('./image/thumbnail.jpg') // JGN DI UBAH!
+global.name = 'Aziz (Owner🗿)', // GNTI NAMA LU!
+global.creator = 'IndraZ' // GANTI NAMA BOT LU BANH!
+global.owner = ['6281380979628'] // GNTI NOMOR LU!
+global.premium = ['628811862408']
+global.quotes = 'Kesempatan Anda Untuk Sukses Di Setiap Kondisi Selalu Dapat Diukur Oleh Seberapa Besar Kepercayaan Anda Pada Diri Sendiri' // Ganti Quotes, Tapi Trsrh Klian
+global.pulsa = ['081528965381'] // UBAH NOMOR YG AKTIF!
+global.botname = 'IndraZ' // GNTI NAMA BOT LU!
+global.packname = 'Nih Stikel Nya Banh' // TRSERAH MO GANTI AP!
+global.author = 'Aziz // GNTI NMA LU!
+global.prefa = ['','!','.','/','#','$',' ']
+global.sp = '⭔' // JGN DI UBAH²! NTR EROR
+global.mess = {
+success: '*[SUCCESS]* SUKSES PERMINTAAN ANDA TERKIRIM!',
+admin: 'Fitur Ini Khusus Untuk Admin Group!',
+botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+prem: 'Fitur Ini Khusus Untuk Premium',
+owner: 'Fitur Ini Khusus Untuk Owner Bot',
+group: 'Fitur Ini Digunakan Hanya Untuk Group!',
+private: 'Fitur Ini Digunakan Hanya Untuk Private Chat!',
+bot: 'Fitur Ini Khusus Pengguna Nomor Bot',
+wait: '*[WAIT]* SEDANG DIPROSES, HARAP TUNGGU SEBENTAR!',
+endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+premium: "Unlimited",
+free: "Unlimited"
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
